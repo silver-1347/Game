1918b09f054eb206212c388bcb9c560435549fca
silver-1347 <mitsuzumi3yu@gmail.com>
2017-04-23 20:56:14 +0900

    GLEWの導入

--- main.cpp
+++ main.cpp
@@ -1,27 +1,47 @@
+#include <cstdlib>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    if (!glfwInit()){
+        std::cerr << "GLFW初期化に失敗" << std::endl;
+        return 1;
+    }
+
+    atexit(glfwTerminate);
+
+    // Core Profileを設定
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    auto window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window){
+        std::cerr << "ウインドウの作成に失敗" << std::endl;
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK){
+        std::cerr << "GLEWの初期化に失敗" << '\n';
+        return 1;
+    }
+
+    //同期タイミング
+    glfwSwapInterval(1);
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)){
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -32,6 +52,5 @@ int main(void)
         glfwPollEvents();
     }
 
-    glfwTerminate();
     return 0;
 }
